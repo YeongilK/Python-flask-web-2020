ba74aeff84c5ffd02f064328cd1b216050b4a632
Yeongil Kim <skyksdc01@gmail.com>
2020-12-15 18:39:25 +0900

    공원 비교분석

--- 02_DataAnalysis/app.py
+++ 02_DataAnalysis/app.py
@@ -3,6 +3,8 @@ from datetime import timedelta, datetime
 from fbprophet import Prophet
 import os
 import pandas as pd 
+import folium
+import json
 import pandas_datareader as pdr
 import matplotlib as mpl 
 import matplotlib.pyplot as plt 
@@ -36,16 +38,6 @@ def before_first_request():
     for i in kosdaq.index:
         kosdaq_dict[kosdaq['종목코드'][i]] = kosdaq['종목명'][i]
 
-    park = pd.read_csv('./static/data/park_result2.csv', sep=',')
-    del park['Unnamed: 0']
-    park.reset_index(drop=True, inplace=True)
-    # key: 공원명, value[0]: 구별, value[1]: 공원면적, value[2]: 위도, value[3]: 경도 인 딕셔너리 생성
-    park_dict = {}
-    gu = park['구별']; area = park['공원면적']
-    lat = park['위도']; lng = park['경도']
-    for i in park.index:
-        park_dict[park['공원명'][i]] = [gu[i], area[i], lat[i], lng[i]]
-
 @app.before_request
 def before_request():
     pass        # 모든 GET 요청을 처리하는 놈에 앞서서 공통적으로 뭔 일을 처리함
@@ -55,10 +47,108 @@ def index():
     menu = {'ho':1, 'da':0, 'ml':0, 'se':0, 'co':0, 'cg':0, 'cr':0, 'st':0, 'wc':0}
     return render_template('main.html', menu=menu, weather=get_weather_main())
 
-@app.route('/park')
+@app.route('/park', methods=['GET', 'POST'])
 def park():
     menu = {'ho':0, 'da':1, 'ml':0, 'se':1, 'co':0, 'cg':0, 'cr':0, 'st':0, 'wc':0}
-    return render_template('park.html', menu=menu, weather=get_weather_main())
+    park_new = pd.read_csv('./static/data/park_info.csv')
+    park_gu = pd.read_csv('./static/data/park_gu.csv')
+    if request.method == 'GET':
+        map = folium.Map(location=[37.5502, 126.982], zoom_start=11)
+        for i in park_new.index:
+            folium.CircleMarker([park_new.lat[i], park_new.lng[i]], 
+                                radius=int(park_new['size'][i]),
+                                tooltip=f"{park_new['공원명'][i]}({int(park_new.area[i])}㎡)",
+                                color='#3186cc', fill_color='#3186cc').add_to(map)
+        html_file = os.path.join(app.root_path, 'static/img/park.html')
+        map.save(html_file)
+        mtime = int(os.stat(html_file).st_mtime)
+        return render_template('park.html', menu=menu, weather=get_weather_main(),
+                                park_list=list(park_new['공원명'].values), 
+                                gu_list = list(park_new['지역'].unique()), mtime=mtime)
+    else:
+        gubun = request.form['gubun']
+        if gubun == 'name':
+            park_name = request.form['name']
+            df = park_new[park_new['공원명'] == park_name].reset_index()
+            park_result = {'name':park_name, 'addr':df['공원주소'][0], 'area':df.area[0], 'desc':df['공원개요'][0]}
+            map = folium.Map(location=[37.5502, 126.982], zoom_start=11)
+            for i in park_new.index:
+                folium.CircleMarker([park_new.lat[i], park_new.lng[i]], 
+                                    radius=int(park_new['size'][i]),
+                                    tooltip=f"{park_new['공원명'][i]}({int(park_new.area[i])}㎡)",
+                                    color='#3186cc', fill_color='#3186cc').add_to(map)
+            folium.CircleMarker([df.lat[0], df.lng[0]], radius=int(df['size'][0]),
+                                    tooltip=f"{df['공원명'][0]}({int(df.area[0])}㎡)",
+                                    color='crimson', fill_color='crimson').add_to(map)
+            html_file = os.path.join(app.root_path, 'static/img/park_res.html')
+            map.save(html_file)
+            mtime = int(os.stat(html_file).st_mtime)
+            return render_template('park_res.html', menu=menu, weather=get_weather_main(),
+                                    park_result=park_result, mtime=mtime)
+        else:
+            gu_name = request.form['gu']
+            df = park_gu[park_gu['구별'] == gu_name].reset_index()
+            park_result2 = {'gu':gu_name, 'gu_area':df['구면적'][0], 
+                            'park_area':df['공원총면적'][0], 
+                            'park_cnt':df['공원수'][0]}
+            select_gu = park_new[park_new['지역'] == gu_name]
+            map = folium.Map(location=[select_gu['lat'].mean(), select_gu['lng'].mean()], zoom_start=12)
+            for i in select_gu.index:
+                folium.CircleMarker([select_gu.lat[i], select_gu.lng[i]], 
+                                    radius=int(select_gu['size'][i])*2,
+                                    tooltip=f"{select_gu['공원명'][i]}({int(select_gu.area[i])}㎡)",
+                                    color='#3186cc', fill_color='#3186cc').add_to(map)
+            html_file = os.path.join(app.root_path, 'static/img/park_res.html')
+            map.save(html_file)
+            mtime = int(os.stat(html_file).st_mtime)
+            return render_template('park_res2.html', menu=menu, weather=get_weather_main(),
+                                    park_result=park_result2, mtime=mtime)
+
+@app.route('/park_gu/<option>')
+def park_gu(option):
+    menu = {'ho':0, 'da':1, 'ml':0, 'se':1, 'co':0, 'cg':0, 'cr':0, 'st':0, 'wc':0}
+    park_new = pd.read_csv('./static/data/park_info.csv')
+    park_gu_new = pd.read_csv('./static/data/park_gu_new.csv')
+    geo_path = './static/data/skorea_municipalities_geo_simple.json'
+    geo_str = json.load(open(geo_path, encoding='utf-8'))
+
+    map = folium.Map(location=[37.5502, 126.982], zoom_start=11, tiles='Stamen Toner')
+    if option == 'area':
+        map.choropleth(geo_data = geo_str,
+                       data = park_gu_new['공원총면적'],
+                       columns = [park_gu_new.index, park_gu_new['공원총면적']],
+                       fill_color = 'PuRd',
+                       key_on = 'feature.id')
+    elif option == 'count':
+        map.choropleth(geo_data = geo_str,
+                       data = park_gu_new['공원수'],
+                       columns = [park_gu_new.index, park_gu_new['공원수']],
+                       fill_color = 'PuRd',
+                       key_on = 'feature.id')
+    elif option == 'area_ratio':
+        map.choropleth(geo_data = geo_str,
+                       data = park_gu_new['공원면적비율'],
+                       columns = [park_gu_new.index, park_gu_new['공원면적비율']],
+                       fill_color = 'PuRd',
+                       key_on = 'feature.id')
+    elif option == 'per_person':
+        map.choropleth(geo_data = geo_str,
+                       data = park_gu_new['인당공원면적'],
+                       columns = [park_gu_new.index, park_gu_new['인당공원면적']],
+                       fill_color = 'PuRd',
+                       key_on = 'feature.id')
+
+    for i in park_new.index:
+        folium.CircleMarker([park_new.lat[i], park_new.lng[i]], 
+                        radius=int(park_new['size'][i]),
+                        tooltip=f"{park_new['공원명'][i]}({int(park_new.area[i])}㎡)",
+                        color='blue', fill_color='blue').add_to(map)
+    html_file = os.path.join(app.root_path, 'static/img/park_gu.html')
+    map.save(html_file)
+    mtime = int(os.stat(html_file).st_mtime)
+    option_dict = {'area':'공원면적', 'count':'공원수', 'area_ratio':'공원면적 비율', 'per_person':'인당 공원면적'}
+    return render_template('park_gu.html', menu=menu, weather=get_weather_main(),
+                            option=option, option_dict=option_dict, mtime=mtime)
 
 @app.route('/stock', methods=['GET', 'POST'])
 def stock():
